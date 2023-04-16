1abb18f5407110cd57fd919184081d4da7940174
SedaNoty <109861733+SedaNoty@users.noreply.github.com>
2023-04-16 20:49:01 +0000

    Deploying to gh-pages from @ SedaNoty/goit-react-hw-08-phonebook@e6829ec88c0a76864c495a17c53a8754c8e87e2e 🚀

---
+++ static/js/266.a1d08106.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var i=n(184),c={container:{minHeight:"calc(50vh)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:400,fontSize:55,textAlign:"center"}};function l(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsxs)("h1",{style:c.title,children:["Welcome phonebook Application"," "]})})}}}]);
+//# sourceMappingURL=266.a1d08106.chunk.js.map
